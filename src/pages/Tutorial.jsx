32e209d37864605d6d653c8d9eb4ac040dc178ff
Mustak-Ali-Virani <viranimustakali@gmail.com>
2024-03-25 13:52:10 +0600

    Update Tutorial.jsx

--- src/pages/Tutorial.jsx
+++ src/pages/Tutorial.jsx
@@ -1,3 +1,11 @@
 export default function Tutorial(){
-    
+    <div>
+        <p>
+            <ol>
+                <li>Insert Login Credentials from the designated device.</li>
+                <li>Check all the boxes listed in the Checklist page that applies.</li>
+                <li>Submit your Response and wait for the approval from the Checker's end.</li>
+            </ol>
+        </p>
+    </div>
 }
